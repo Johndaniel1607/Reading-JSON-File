1cdb4abfcae8a9a7181c878e1d506b506c6ed148
DESKTOP-6PP8ISQ\TOSHIBA <jrdeleon@csmbataan.ph.education>
2020-11-27 13:14:58 +0800

    TVLICT201.JSONReader

---
+++ TVLICT201.JSONReader/TVLICT201.JSONReader.Windows/Typhoon/Typhoon.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TVLICT201.JSONReader.Windows.Typhoon
+{
+    public class Typhoon
+    {
+
+        public string id { get; set; }
+        public string name { get; set; }
+        public string date { get; set; }
+        public string pressure { get; set; }
+        public string winds { get; set; }
+
+    }
+}
---
+++ TVLICT201.JSONReader/TVLICT201.JSONReader.Windows/Typhoon/TyphoonWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace TVLICT201.JSONReader.Windows.Typhoon
+{
+    /// <summary>
+    /// Interaction logic for TyphoonWindow.xaml
+    /// </summary>
+    public partial class TyphoonWindow : Window
+    {
+        public TyphoonWindow()
+        {
+            InitializeComponent();
+            Typhoon typhoon = new Typhoon();
+
+            using (StreamReader r = new StreamReader("data\\typhoon.json"))
+            {
+                string json = r.ReadToEnd();
+                typhoon = JsonConvert.DeserializeObject<Typhoon>(json);
+            }
+            lblTyphoonId.Content = "Id :" + typhoon.id;
+            lblTyphoonName.Content = "Name :" + typhoon.name;
+            lblTyphoonDate.Content = "Date :" + typhoon.date;
+            lblTyphoonPressure.Content = "Pressure :" + typhoon.pressure;
+            lblTyphoonWinds.Content = "Winds :" + typhoon.winds;
+        }
+    }
+}
